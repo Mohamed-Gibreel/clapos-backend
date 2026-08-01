82bc1afe98cd2f44957aa0c8075738a860de3e05
Mohamed Gibreel <m.adilgibreel@gmail.com>
2026-08-01 12:11:43 +0400

    chore: add TypeORM CLI datasource config for migration generation

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,36 +1,15 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
-import {
-  POSTGRES_DB,
-  POSTGRES_HOST,
-  POSTGRES_USER,
-  POSTGRES_PORT,
-  POSTGRES_PASSWORD,
-} from 'src/utils/constants';
-config();
 
-const configService = new ConfigService();
+dotenv.config();
 
-const host = configService.get(POSTGRES_HOST);
-const port = configService.get(POSTGRES_PORT);
-
-const database = configService.get(POSTGRES_DB);
-const username = configService.get(POSTGRES_USER);
-const password = configService.get(POSTGRES_PASSWORD);
-
-const AppDataSource = new DataSource({
+export default new DataSource({
   type: 'postgres',
-  host: host,
-  port: port,
-  username: username,
-  password: password,
-  database: database,
-  synchronize: false,
-  entities: ['**/*.entity.ts'],
-  migrations: ['src/database/migrations/*-migration.ts'],
-  migrationsRun: false,
-  logging: true,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/database/migrations/*.ts'],
 });
-
-export default AppDataSource;
